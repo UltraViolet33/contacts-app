9515d9009b8b805c68387fcd5a98da4bf822fd69
UltraViolet33 <ulysse.valdenairepro@gmail.com>
2022-03-24 15:17:28 +0100

    add Input.js

---
+++ components/Input.js
@@ -0,0 +1,27 @@
+import { View, StyleSheet, TextInput } from "react-native";
+
+export default Input = ({ onChange, placeHold, valueInput }) => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        onChangeText={onChange}
+        placeholder={placeHold}
+        value={valueInput}
+        style={styles.input}
+      ></TextInput>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    margin: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    height: 50,
+    borderRadius: 20,
+    padding: 10,
+    borderWidth: 2,
+  },
+});
